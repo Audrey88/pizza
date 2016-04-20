d103b5c76d245f92839a8b54c71f91df0b00626e
Audrey88 <audrey.toussaints@gmail.com>
2016-04-20 09:54:37 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<div id="Nom"> <h1> Tour de Pisa! <h1> </div>
+</head>
+
+<body>
+
+	<div id="logo">(logo) </div>
+
+	<div id="list"> 
+		<h3>Nos pizzas </h3>
+</div>
+
+	<div id="open"> 
+
+		<p> ouvert: 7/24 </p>
+
+</div>
+
+	<div id="command"> <h3> Commandez! <h3>
+ </div>
+
+	<div id="stefano"> <img src="public/Cuisto">
+ </div>
+	
+	<div id="text"> <p> Bienvenue à la Tour de Pisa! </br>Stefano et son équipe de gladiateur vous accuillerons dans ce restaurant d'ambiance greco-romaine et devenez combattant contre votre ennemi de toujours... la faim! Venez savourez nos pizzas qui réveilleront vos papilles avec vos amis ou en famille!</br>
+Vous pourrez nous contacter et nous rendre visite à cette adresse: </br>
+1 rue de Pise 01173 Toscane.</br>
+Téléphone: 03.54.43.96.42 </br>
+
+</p>
+</div>
+
+	<div id="box"> <h4>pizza du mois<h4> </div>
+
+	<div id="book"><h4> livre d'or<h4>
+<p> Laissez-nous vos commentaires <p> </div>
+
+
+	<div id="command"> </div>
+	
+	<div id="photo"> </div>
+
+	<div id="nom"> </div>
+
+	<div id="ingredient"> </div>
+
+	<div id="size"> </div>
+
+	<div id="prix"> </div>
+
+	<div id="command"> </div>
+
+
+	<div id="phone"> </div>
+
+	<div id="valid"> validez commande </div>
+
+	<div id="cb"> <h4> Carte Bancaire </h4> </div>
+
+	<div id="pay"> <h4> Paiement Paypal </h4> </div>
+
+	<div id="lcommand"> <h5> Votre liste de commande </h5> </div>
+	 
+
+</body>
+
+
+</html>
