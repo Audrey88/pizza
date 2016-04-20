d1c869ad61f90b3745b243366c264370155bcf2d
Nirgalz <znirgal@gmail.com>
2016-04-20 16:42:34 +0200

    updated php script

--- index.php
+++ index.php
@@ -1,30 +1,46 @@
 <!DOCTYPE html>
 <html>
 <head>
-	
+	<script language="javascript"> function liste(form5) { 
+alert("L\'élément " + (form5.list.selectedIndex + 1)); }
+</SCRIPT> 
 </head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width">
 	<link rel="stylesheet" type="text/css" href="style.css">
 <body>
 <!-- php script -->
-<table border=1><th>Pizzas</th>
-                        <tbody>
+<table border=1>
+<th>Pizzas</th>
+<tbody>
 <?php 
         $fichier = 'res/catalogue-pizzas.xml';
         $xml = simplexml_load_file($fichier);
 
 foreach ($xml as $pizza) {
 	$picz = $pizza->image;
-	echo $pizza->prix->attributes();
+	$nom = $pizza->nom;
+	$comp = $pizza->composition;
+	$l = $pizza->prix->attributes()->{'taille_l'};
+	$xl = $pizza->prix->attributes()->{'taille_xl'};
+	$xxl = $pizza->prix->attributes()->{'taille_xxl'};
 
-        echo 
-                "<tr><td><img src='$picz' width='160' height='120'></td>",
-                "<td>".$pizza->nom."</td>",
-                "<td>".$pizza->composition."</td></tr>",
-                "<tr><td>".$pizza->prix->attributes()->{'taille_l'}."</td></tr>",
-                "<tr><td>".$pizza->prix->attributes()->{'taille_xl'}."</td></tr>",
-                "<tr><td>".$pizza->prix->attributes()->{'taille_xxl'}."</td></tr>";
+        echo    "<tr>
+                <td><img src='$picz' width='160' height='120'></td>
+                <td>$nom</td>
+                <td>$comp</td>
+                <td>
+
+<FORM NAME='form5'> 
+<SELECT NAME='list'> 
+<OPTION VALUE='1'>Taille L : $l euros
+<OPTION VALUE='2'>Taille XL : $xl euros
+<OPTION VALUE='3'>Taille XXL : $xxl euros
+</SELECT> 
+<INPUT TYPE='button'NAME='b' VALUE='OK' onClick='liste(form5)'> </FORM>
+
+                </tr>";
+
 
     }
 ?>
